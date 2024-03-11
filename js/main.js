9f7fb7c621976a69acba7dd743a3e0afb880e298
unknown <rikofsaputra000@gmail.com>
2024-03-11 23:45:40 +0700

    selesai

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+// SCROLL-NAV
 const navbar = document.getElementById("navbar");
 
 window.addEventListener("scroll", function(){
     navbar.classList.toggle("nav-scroll", this.window.scrollY > 10)
 })
+
+// MENU-BAR
+
+function hideSide() {
+    const menu = document.querySelector(".sidebar");
+    menu.style.display = "none"
+}
+function showSide() {
+    const menu = document.querySelector(".sidebar");
+    menu.style.display = "flex"
+}
